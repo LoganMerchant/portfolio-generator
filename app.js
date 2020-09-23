09b775f6bdaa7d2af80ca18b181b6d004b59eca3
Logan Merchant <merchantclogan@gmail.com>
2020-09-22 22:43:16 -0600

    add fs module, generated html"

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // =========================== LESSON ONE ================================
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
@@ -27,7 +29,7 @@ const generatePage = (userName, githubName) => {
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name = "viewport" content="width=device-width", initial-scale=1.0">
+        <meta name = "viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie-edge">
         <title>Portfolio Demo</title>
     </head>
@@ -40,5 +42,8 @@ const generatePage = (userName, githubName) => {
     `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out "index.html" to see the output!');
+});
